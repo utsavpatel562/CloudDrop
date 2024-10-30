3a439379de47f56daf92c217e20d96ce8e91fd22
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-10-30 18:48:40 -0400

    new commit

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+      <section className="justify-center place-content-center flex p-10 h-screen bg-slate-100">
+        <div className="flex place-content-center justify-center p-10 bg-gray-900 rounded-lg md:h-fit">
+          <SignUp />
+        </div>
+      </section>
+    </>
+  );
 }
