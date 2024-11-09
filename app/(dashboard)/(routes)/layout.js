41b97361249ae5f4539390118f15aa39e8d2bc7b
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-09 12:04:28 -0500

    new commit

--- app/(dashboard)/(routes)/layout.js
+++ app/(dashboard)/(routes)/layout.js
@@ -5,10 +5,10 @@ function layout({children}) {
   return (
     <>
     <div>
-    <div className='h-full w-64 flex-col fixed inset-y-0 z-50'>
+    <div className='hidden h-full md:w-64 flex-col fixed inset-y-0 z-50'>
       <Sidenav/>
     </div>
-    <div className='ml-64'>
+    <div className='md:ml-64'>
     {children}
     </div>
     </div>
--- app/(dashboard)/_components/Sidenav.js
+++ app/(dashboard)/_components/Sidenav.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { MdOutlineUploadFile } from "react-icons/md";
+import { LuFileSearch2 } from "react-icons/lu";
+import { TbPremiumRights } from "react-icons/tb";
 
 function Sidenav() {
   // sidenav and  main content
-
+  const menuList = [
+    {
+      id: 1,
+      name: 'Upload',
+      icon: MdOutlineUploadFile,
+      path: '/upload',
+    },
+    {
+      id: 2,
+      name: 'Files',
+      icon: LuFileSearch2,
+      path: '/files',
+    },
+    {
+      id: 3,
+      name: 'Upgrade',
+      icon: TbPremiumRights,
+      path: '/upgrade',
+    },
+  ]
   return (
     <>
     <div>Sidenav</div>
