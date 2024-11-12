f9c5ae2cc8b8a6fe4700710e6e9ac90d7aaedb36
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-12 15:25:51 -0500

    new commit

--- app/(dashboard)/(routes)/layout.js
+++ app/(dashboard)/(routes)/layout.js
@@ -6,10 +6,10 @@ function layout({children}) {
   return (
     <>
     <div className='bg-zinc-800 h-screen'>
-    <div className='h-full w-64 flex-col fixed inset-y-0 z-50 bg-zinc-900'>
+    <div className='h-full md:w-64 flex-col fixed inset-y-0 z-50 bg-zinc-900 md:flex hidden'>
       <Sidenav/>
     </div>
-    <div className='ml-64'>
+    <div className='md:ml-64'>
       <TopHeader/>
     {children}
     </div>
