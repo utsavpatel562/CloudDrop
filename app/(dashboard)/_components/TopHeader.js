e242bf530c2546ded4b9c3f753b195207c8b168d
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-12 15:58:05 -0500

    new commit

--- app/(dashboard)/_components/TopHeader.js
+++ app/(dashboard)/_components/TopHeader.js
@@ -1,16 +1,25 @@
 "use client";
 import { UserButton, useUser } from '@clerk/nextjs';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { FiAlignJustify } from "react-icons/fi";
+import { TbHome } from "react-icons/tb";
 
 function TopHeader() {
+    const router = useRouter();
+    const handleHome = () => {
+        router.push('/');
+      };
     return (
+        <>
         <div className="flex items-center justify-between p-5 border-b border-zinc-700 shadow-sm gap-4 md:justify-end">
+        <button className='md:flex hidden bg-zinc-900 p-1 pr-4 pl-4 rounded-md items-center gap-2' onClick={handleHome}><TbHome className='w-4 h-4'/>Home</button>
             <FiAlignJustify className="text-xl cursor-pointer md:hidden" />
            <Image src={"/logo2.png"} alt='logo' width={200} height={200} className='md:hidden'/>
             <UserButton />
         </div>
+        </>
     );
 }
 
