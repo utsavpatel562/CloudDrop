a0b1c8312c851d3088e4d27fbb4772edf5a2f191
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-13 08:45:25 -0500

    first commit

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { RiFolderUploadLine } from "react-icons/ri";
 function UploadForm() {
   return (
     <div>
-        UploadForm
+      
+<div className="flex items-center justify-center w-full mt-10">
+    <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-zinc-700 hover:bg-zinc-800 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-zinc-800">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <RiFolderUploadLine className='w-9 h-9 mb-5'/>
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">Any files (MAX. 800x400px)</p>
+        </div>
+        <input id="dropzone-file" type="file" className="hidden" />
+    </label>
+</div> 
+
     </div>
   )
 }
