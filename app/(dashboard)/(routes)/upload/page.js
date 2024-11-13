2fbdfa031ccf43ee3aa64ebac340baa9efcb9339
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-13 08:33:31 -0500

    new commit

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -4,7 +4,8 @@ import UploadForm from './_components/UploadForm';
 function Upload() {
   return (
     <>
-    <div>
+    <div className='p-5 px-8 md:px-28'>
+      <h2 className='md:text-4xl text-center m-5'>Start <strong className='text-green-500'>Uploading</strong> File and <strong className='text-green-500'>Share</strong> it</h2>
       <UploadForm/>
     </div>
     </>
--- app/(dashboard)/_components/TopHeader.js
+++ app/(dashboard)/_components/TopHeader.js
@@ -14,7 +14,7 @@ function TopHeader() {
     return (
         <>
         <div className="flex items-center justify-between p-5 border-b border-zinc-700 shadow-sm gap-4 md:justify-end">
-        <button className='md:flex hidden bg-zinc-900 p-1 pr-4 pl-4 rounded-md items-center gap-2' onClick={handleHome}><TbHome className='w-4 h-4'/>Home</button>
+        <button className='md:flex hidden items-center gap-2' onClick={handleHome}><TbHome className='w-4 h-4'/>Home</button>
             <FiAlignJustify className="text-xl cursor-pointer md:hidden" />
            <Image src={"/logo2.png"} alt='logo' width={200} height={200} className='md:hidden'/>
             <UserButton />
