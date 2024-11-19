d4f73b2f2e99b303f42461208fbe12201c43afd3
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-19 14:18:42 -0500

    first commit

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import { GrUploadOption } from "react-icons/gr";
 import { RiFolderUploadLine } from "react-icons/ri";
 function UploadForm() {
   return (
-    <div>
+    <div className='text-center'>
       
 <div className="flex items-center justify-center w-full mt-10">
     <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-zinc-700 hover:bg-zinc-800 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-zinc-800">
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <RiFolderUploadLine className='w-9 h-9 mb-5'/>
-            <p className="mb-2 text-md text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or <strong className='text-green-500'>drag</strong> and <strong className='text-green-500'>drop</strong></p>
-            <p className="text-sm text-gray-500 dark:text-gray-400">Any files (MAX. 800x400px)</p>
+            <RiFolderUploadLine className='w-12 h-12 mb-5'/>
+            <p className="mb-2 md:text-lg text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or <strong className='text-green-500'>drag</strong> and <strong className='text-green-500'>drop</strong></p>
+            <p className="md:text-md text-gray-500 dark:text-gray-400">Any files (MAX. 800x400px)</p>
         </div>
         <input id="dropzone-file" type="file" className="hidden" />
     </label>
 </div> 
-
+<button className='flex items-center justify-center m-auto gap-2 p-3 font-bold bg-green-600 text-white w-[30%] rounded-full mt-5 disabled:bg-zinc-800 disabled:text-zinc-800'><GrUploadOption/>Upload</button>
     </div>
   )
 }
