99c3e91ee1d88c16bce6add7ca2272a530e699cb
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-23 16:02:40 -0500

    first commit

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -4,6 +4,14 @@ import { GrUploadOption } from "react-icons/gr";
 import { RiFolderUploadLine } from "react-icons/ri";
 function UploadForm() {
   const [file, setFile] = useState();
+  const onFileSelect = (file)=> {
+    console.log(file);
+    if(file&&file.size>2000000){
+      console.log("The size is greater than 2 MB");
+      return;
+    }
+    setFile(file);
+  }
   return (
     <div className='text-center'>
       
@@ -12,12 +20,12 @@ function UploadForm() {
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <RiFolderUploadLine className='w-12 h-12 mb-5'/>
             <p className="mb-2 md:text-lg text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or <strong className='text-green-500'>drag</strong> and <strong className='text-green-500'>drop</strong></p>
-            <p className="md:text-md text-gray-500 dark:text-gray-400">Any files (MAX. 800x400px)</p>
+            <p className="md:text-md text-gray-500 dark:text-gray-400">Any files (MAX Size: 2MB)</p>
         </div>
-        <input id="dropzone-file" type="file" className="hidden" onChange={(event)=> setFile(event.target.files[0])} />
+        <input id="dropzone-file" type="file" className="hidden" onChange={(event)=> onFileSelect(event.target.files[0])} />
     </label>
 </div> 
-<button disabled className='flex items-center justify-center m-auto gap-2 p-3 font-bold bg-green-600 text-white w-[30%] rounded-full mt-5 disabled:bg-zinc-900 disabled:text-zinc-700'><GrUploadOption/>Upload</button>
+<button disabled={!file} className='flex items-center justify-center m-auto gap-2 p-3 font-bold bg-green-600 text-white w-[30%] rounded-full mt-5 disabled:bg-zinc-900 disabled:text-zinc-700'><GrUploadOption/>Upload</button>
     </div>
   )
 }
