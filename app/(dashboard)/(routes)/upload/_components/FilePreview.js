fb12b0e738a2e45adc8f5910c9407721d9730a2a
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-24 12:53:46 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/upload/_components/FilePreview.js
+++ app/(dashboard)/(routes)/upload/_components/FilePreview.js
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 function FilePreview({file}) {
   return (
-    <div className='flex items-center md:w-full gap-2 pt-3'>
-        <Image src={"/file2.png"} width={80} height={80} alt='File' className='bg-zinc-700 p-2 rounded-md'/>
-        <div className='md:w-full'>
-        <h2 className='md:text-lg bg-zinc-700 text-zinc-300 p-1 rounded-md'>{file.name}</h2>
-        <h2 className='md:text-lg bg-zinc-700 p-1 text-zinc-300 rounded-md mt-2'>{file?.type} / {(file.size/1024/1024).toFixed(2) + " MB"}</h2>
+    <div className='flex items-center md:w-full gap-2 justify-between p-3 bg-zinc-900 my-2 rounded-lg'>
+      <div className='flex items-center md:w-full gap-4'>
+        <Image src={"/file2.png"} width={60} height={60} alt='File' className='bg-zinc-700 p-2 rounded-md'/>
+        <div className='text-zinc-300 text-left md:text-md'>
+        <h2 className=''>{file.name}</h2>
+        <h2 className='mt-1'>{file?.type} / {(file.size/1024/1024).toFixed(2) + " MB"}</h2>
         </div>
+        </div>
+        <IoIosCloseCircleOutline className='md:w-7 md:h-7'/>
     </div>
   )
 }
