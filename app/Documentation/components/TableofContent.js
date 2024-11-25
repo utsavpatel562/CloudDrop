7446015df8b57480ea01b15a4b63c8fe8fdfbc38
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-25 12:44:40 -0500

    NEW COMMIT

--- app/Documentation/components/TableofContent.js
+++ app/Documentation/components/TableofContent.js
@@ -11,7 +11,7 @@ function TableofContent() {
 <div className="rounded-lg flex items-center flex-wrap mt-3">
   <ul className="flex items-center">
 	<li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-200 hover:text-blue-500">
+	  <a href="#" className="text-zinc-200 hover:text-green-500">
 		<FaCircleArrowRight/>
 	  </a>
 
@@ -19,7 +19,7 @@ function TableofContent() {
 	</li>
 
 	<li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		Introduction
 	  </a>
 
@@ -27,28 +27,28 @@ function TableofContent() {
 	</li>
 
 	<li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		Features
 	  </a>
 
 	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
 	</li>
     <li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		Technology Used
 	  </a>
 
 	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
 	</li>
     <li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		Getting Started
 	  </a>
 
 	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
 	</li>
     <li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		API Documentation
 	  </a>
 
@@ -56,11 +56,32 @@ function TableofContent() {
 	</li>
 
 	<li className="inline-flex items-center">
-	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+	  <a href="#" className="text-zinc-400 hover:text-green-500">
 		Contact
 	  </a>
 	</li>
   </ul>
+</div>
+<div id='Introduction'>
+	<h2 className='font-extrabold text-xl mt-10'>1. Introduction</h2>
+	<p className='mt-1 text-zinc-400'>CloudDrop is a modern file-sharing application enabling seamless 
+		and secure transfer of files between users. Built for scalability and high availability, it ensures 
+		privacy and efficiency in handling file uploads, downloads, and management. Developed using Next.js, React.js, Tailwind CSS, Firebase, Auth. using Clerk.</p>
+</div>
+<div id='Features'>
+<h2 className='font-extrabold text-xl mt-10'>2. Features</h2>
+</div>
+<div id='TechnologyUsed'>
+<h2 className='font-extrabold text-xl mt-10'>3. Technology Used</h2>
+</div>
+<div id='GettingStarted'>
+<h2 className='font-extrabold text-xl mt-10'>4. Getting Started</h2>
+</div>
+<div id='APIDocumentation'>
+<h2 className='font-extrabold text-xl mt-10'>5. API Documentation</h2>
+</div>
+<div id='Contact'>
+<h2 className='font-extrabold text-xl mt-10'>6. Contact</h2>
 </div>
         </div>
     </div>
