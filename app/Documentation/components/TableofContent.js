58d4f2b0e1e3ab0556186bd098a4f751e7a4b682
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-25 11:23:07 -0500

    NEW COMMIT

--- app/Documentation/components/TableofContent.js
+++ app/Documentation/components/TableofContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaRegFileCode } from "react-icons/fa6";
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 function TableofContent() {
   return (
@@ -7,6 +8,60 @@ function TableofContent() {
         <div className='bg-zinc-800 p-9'>
             <h1 className='text-zinc-300 font-semibold md:text-3xl flex items-center gap-2'><FaRegFileCode className='md:w-7 md:h-7'/>Documentation</h1>
             <p className='mt-1'>A robust file-sharing platform designed for efficiency, scalability, and user-friendliness.</p>
+<div className="rounded-lg flex items-center flex-wrap mt-3">
+  <ul className="flex items-center">
+	<li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-200 hover:text-blue-500">
+		<FaCircleArrowRight/>
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-200 font-medium">/</span>
+	</li>
+
+	<li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		Introduction
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
+	</li>
+
+	<li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		Features
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
+	</li>
+    <li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		Technology Used
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
+	</li>
+    <li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		Getting Started
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
+	</li>
+    <li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		API Documentation
+	  </a>
+
+	  <span className="mx-4 h-auto text-zinc-400 font-medium">/</span>
+	</li>
+
+	<li className="inline-flex items-center">
+	  <a href="#" className="text-zinc-400 hover:text-blue-500">
+		Contact
+	  </a>
+	</li>
+  </ul>
+</div>
         </div>
     </div>
   )
