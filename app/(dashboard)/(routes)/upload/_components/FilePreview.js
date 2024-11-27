9ffc3c47813e55b82ae51837fc430e7c67058a01
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-27 16:35:26 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/upload/_components/FilePreview.js
+++ app/(dashboard)/(routes)/upload/_components/FilePreview.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
-function FilePreview({file}) {
+function FilePreview({file, removeFile}) {
   return (
     <div className='flex items-center md:w-full gap-2 justify-between p-3 bg-zinc-900 my-2 rounded-lg'>
       <div className='flex items-center md:w-full gap-4'>
@@ -12,7 +12,7 @@ function FilePreview({file}) {
         <h2 className='mt-1'>{file?.type} / {(file.size/1024/1024).toFixed(2) + " MB"}</h2>
         </div>
         </div>
-        <IoIosCloseCircleOutline className='md:w-7 md:h-7 cursor-pointer'/>
+        <IoIosCloseCircleOutline className='md:w-7 md:h-7 cursor-pointer' onClick={()=>removeFile()}/>
     </div>
   )
 }
--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -31,7 +31,7 @@ function UploadForm() {
     </label>
 </div> 
 {errorMsg?<AlertMsg/>:null}
-{file?<FilePreview file={file}/>:null}
+{file?<FilePreview file={file} removeFile={()=>setFile(null)}/>:null}
 <button disabled={!file} className='flex items-center justify-center m-auto gap-2 p-3 font-bold bg-green-600 text-white w-[30%] rounded-full mt-5 disabled:bg-zinc-900 disabled:text-zinc-700'><GrUploadOption/>Upload</button>
     </div>
   )
