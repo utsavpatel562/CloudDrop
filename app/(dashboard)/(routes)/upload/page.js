b92eb492ae2d21bc3f393c9efa68c3ac74eff583
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-27 17:35:15 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,13 +1,19 @@
 "use client"
 import React from 'react'
 import UploadForm from './_components/UploadForm';
+import { app } from '@/firebaseConfig';
+import { getStorage } from "firebase/storage";
 
 function Upload() {
+  const storage=getStorage(app)
+  const uploadFile=(file)=> {
+
+  }
   return (
     <>
     <div className='p-5 px-8 md:px-28'>
       <h2 className='md:text-4xl text-center m-5'>Start <strong className='text-green-500'>Uploading</strong> File and <strong className='text-green-500'>Share</strong> it</h2>
-      <UploadForm uploadBtnClick={()=>console.log(file)}/>
+      <UploadForm uploadBtnClick={(file)=>uploadFile(file)}/>
     </div>
     </>
   )
