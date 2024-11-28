dfaeb7636e4725950aa3636dd8c94e82476746f2
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-11-28 18:26:14 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/upload/_components/ProgressBar.js
+++ app/(dashboard)/(routes)/upload/_components/ProgressBar.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 function ProgressBar({progress=40}) {
   return (
-    <div className='bg-zinc-400 w-full h-2 mt-3 rounded-full'>
-        <div className='p-1 bg-green-500 rounded-full' style={{
-            width: `${progress}%`
-        }}></div>
+    <div className='bg-zinc-700 w-full mt-3 rounded-full' style={{
+        boxShadow: "0px 0px 8px 0px white inset"
+    }}>
+        <div className='p-0.2 bg-green-600 font-bold rounded-full' style={{
+            width: `${progress}%`,
+            boxShadow: "0px 0px 4px 0px green inset"
+        }}>{`${Number(progress).toFixed(0)}%`}</div>
     </div>
   )
 }
