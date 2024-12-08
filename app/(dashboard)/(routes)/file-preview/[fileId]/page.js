5d1572e793c63e32b2a3a8ded004fd1abc65b1f4
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-08 11:53:32 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/file-preview/[fileId]/page.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/page.js
@@ -1,10 +1,20 @@
 "use client"
+import { app } from '@/firebaseConfig';
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
 import React, { useEffect } from 'react'
 
 function FilePreview({params}) {
+  const db = getFirestore(app);
   useEffect(()=> {
     console.log(params?.fileDocId)
   }, [])
+
+ // To Get the file from the Firestore
+ const getFileInfo = async() => {
+  const docRef = doc(db, "uploadedFile", params?.fileDocId);
+  const docSnap = await getDoc(docRef)
+ }
+
   return (
     <div>FilePreview</div>
   )
