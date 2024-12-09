3bd6090b7df9ccf74a3a99ad9937d1527823dcc8
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-09 09:39:13 -0500

    NEW COMMIT

---
+++ app/(dashboard)/(routes)/moreapps/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IoIosApps } from "react-icons/io";
+function MoreApps() {
+  return (
+    <>
+    <div>
+    <div className='p-6'>
+        <h2 className='text-2xl font-semibold flex items-center gap-1'><IoIosApps/>Explore my top apps</h2>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default MoreApps;
