0fc8db00eb32a77395261c761e7e451b108e4607
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-09 09:53:43 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/moreapps/page.js
+++ app/(dashboard)/(routes)/moreapps/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { IoIosApps } from "react-icons/io";
+import MyProject from './_components/MyProject';
 function MoreApps() {
   return (
     <>
     <div>
     <div className='p-6'>
-        <h2 className='text-2xl font-semibold flex items-center gap-1'><IoIosApps/>Explore my top apps</h2>
+        <h2 className='md:text-2xl font-semibold flex items-center gap-2'><IoIosApps/>Explore my top apps</h2>
+    <MyProject/>
     </div>
     </div>
     </>
