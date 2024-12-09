2f19bb4525a63e165952726d6bfc5b7ffd49a3fa
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-09 10:26:08 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/moreapps/_components/MyProject.js
+++ app/(dashboard)/(routes)/moreapps/_components/MyProject.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 function MyProject() {
   return (
-    <div>MyProject</div>
+    <div className='my-5'>
+        <div className='grid md:grid-cols-3 gap-3'>
+            <div className='border border-r-slate-100'>
+            <div className="md:w-[400px] md:h-[200px] bg-white rounded-lg overflow-hidden" style={{
+                boxShadow: "0px 0px 90px -43px rgba(173, 173, 173, 1)"
+            }}>
+            <img src="/projectimg2.png" className="w-full h-full object-cover" alt="projectimg2" />
+            </div>
+            CarrerBoost AI
+            </div>
+
+        {/* Second */ }
+        <div className='border border-r-slate-100'>
+            <div className="md:w-[400px] md:h-[200px] bg-white rounded-lg overflow-hidden" style={{
+                boxShadow: "0px 0px 90px -43px rgba(173, 173, 173, 1)"
+            }}>
+            <img src="/projectimg2.png" className="w-full h-full object-cover" alt="projectimg2" />
+            </div>
+            CarrerBoost AI
+            </div>
+
+            
+        </div>
+    </div>
   )
 }
 
