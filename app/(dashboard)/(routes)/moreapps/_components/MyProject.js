f5a187751257b3bad391ae732afa2cf116734604
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-10 14:01:56 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/moreapps/_components/MyProject.js
+++ app/(dashboard)/(routes)/moreapps/_components/MyProject.js
@@ -5,11 +5,11 @@ function MyProject() {
     <div className="my-5 px-4">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
         {/* CareerBoost AI */}
-        <div className="text-center">
+        <div className="bg-zinc-950 p-4 rounded-lg">
           <div
             className="w-full h-48 md:h-56 lg:h-64 bg-white rounded-lg overflow-hidden mx-auto"
             style={{
-              boxShadow: '0px 0px 90px -43px rgba(173, 173, 173, 1)',
+              boxShadow: '0px 0px 100px -43px rgba(173, 173, 173, 1)',
             }}
           >
             <img
@@ -18,15 +18,18 @@ function MyProject() {
               alt="CareerBoost AI"
             />
           </div>
-          <p className="mt-3 font-medium text-lg">CareerBoost AI</p>
+          <h2 className="mt-4 font-semibold text-lg text-white">CareerBoost AI</h2>
+          <p className="mt-2 text-justify text-gray-300">
+            Full Stack AI Mock Interview App project! A powerful AI-driven mock interview application using modern technologies.
+          </p>
         </div>
 
         {/* Zoom Clone */}
-        <div className="text-center">
+        <div className="bg-zinc-950 p-4 rounded-lg">
           <div
             className="w-full h-48 md:h-56 lg:h-64 bg-white rounded-lg overflow-hidden mx-auto"
             style={{
-              boxShadow: '0px 0px 90px -43px rgba(173, 173, 173, 1)',
+              boxShadow: '0px 0px 100px -43px rgba(173, 173, 173, 1)',
             }}
           >
             <img
@@ -35,15 +38,18 @@ function MyProject() {
               alt="Zoom Clone"
             />
           </div>
-          <p className="mt-3 font-medium text-lg">Zoom Clone</p>
+          <h2 className="mt-4 font-semibold text-lg text-white">Zoom Clone</h2>
+          <p className="mt-2 text-justify text-gray-300">
+          A feature-rich Zoom clone built with modern tech stack such as TypeScript, Next.js, Tailwind CSS, GetStream, Clerk and etc.
+          </p>
         </div>
 
         {/* CarConnect */}
-        <div className="text-center">
+        <div className="bg-zinc-950 p-4 rounded-lg">
           <div
             className="w-full h-48 md:h-56 lg:h-64 bg-white rounded-lg overflow-hidden mx-auto"
             style={{
-              boxShadow: '0px 0px 90px -43px rgba(173, 173, 173, 1)',
+              boxShadow: '0px 0px 100px -43px rgba(173, 173, 173, 1)',
             }}
           >
             <img
@@ -52,7 +58,10 @@ function MyProject() {
               alt="CarConnect"
             />
           </div>
-          <p className="mt-3 font-medium text-lg">CarConnect</p>
+          <h2 className="mt-4 font-semibold text-lg text-white">CarConnect</h2>
+          <p className="mt-2 text-justify text-gray-300">
+          This full-stack application is designed with performance, scalability, and user engagement in mind. It leverages a modern tech stack.
+          </p>
         </div>
       </div>
     </div>
