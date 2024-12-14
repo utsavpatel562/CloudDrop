ff7acd8ce50407d1c218099a1c3d2b464db802c2
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2024-12-13 19:04:51 -0500

    NEW COMMIT

--- app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.js
@@ -1,33 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { MdContentCopy } from "react-icons/md";
+import { BiLock } from "react-icons/bi";
+function FileShareForm({ file, onPasswordSave }) {
+  const [isPasswordEnable, setIsEnablePassword] = useState(false);
+  const [password, setPassword] = useState('');
 
-function FileShareForm({file, onPasswordSave}) {
-  const [isPasswordEnable, setIsEnablePassword]=useState(false);
-  const [password, setPassword]=useState('');
-  return file&&(
+  return file && (
     <>
-    <div className='flex flex-col gap-2 md:mt-4'>
-      <div>
-        <label className='md:text-md text-zinc-300 font-semibold'>Short Url</label>
-        <div className='flex gap-5 p-2 rounded-md justify-between mt-2 bg-zinc-900'>
-          <input type='text' value={file.shortUrl} disabled className='disabled:text-zinc-400 outline-none w-full bg-transparent md:pl-1'/> 
-          <MdContentCopy className='md:w-6 md:h-6'/>
+      <div className='flex flex-col gap-2 md:mt-4'>
+        <div>
+          <label className='md:text-md text-zinc-300 font-semibold'>Short Url</label>
+          <div className='flex gap-5 p-2 rounded-md justify-between mt-2 bg-zinc-900'>
+            <input
+              type='text'
+              value={file.shortUrl}
+              disabled
+              className='disabled:text-zinc-400 outline-none w-full bg-transparent md:p-1 md:pl-1'
+            />
+            <MdContentCopy className='md:w-6 md:h-6' />
+          </div>
         </div>
-      </div>
-      <div className='gap-3 flex mt-5'>
-        <input type='checkbox' onChange={(e)=>setIsEnablePassword}/>
-        <label>Enable password</label>
-      </div>
-{isPasswordEnable?
-      <div className='flex gap-3 items-center'>
-        <div className='border rounded-md w-full p-2'>
-          <input className='disabled:bg-gray-300 hover:bg-blue-300' type='password' onChange={(e)=>setPassword(e.target.value)}/>
+        <div className='gap-3 flex mt-5'>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsEnablePassword(e.target.checked)} // Corrected state update
+          />
+          <label>Enable password</label>
         </div>
-        <button className='p-2 bg-green-600 text-white rounded-md disabled:bg-gray-300 hover:bg-blue-400' disabled={password?.length<3} onClick={()=>onPasswordSave(password)}>Save</button>
-      </div>:null}
-    </div>
+        {isPasswordEnable ? (
+          <div className='flex gap-3 items-center'>
+            <div className='rounded-md w-full p-2 bg-zinc-900'>
+              <input
+                className='bg-transparent md:p-1 md:pl-1 text-zinc-400'
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Type your password'
+              />
+            </div>
+            <button
+              className='flex items-center gap-1 p-2 md:pr-4 md:pl-4 bg-green-600 text-white rounded-md disabled:bg-transparent disabled:text-transparent hover:bg-green-500'
+              disabled={password?.length < 3}
+              onClick={() => onPasswordSave(password)}
+            >
+              Save
+              <BiLock className='md:w-4 md:h-4'/>
+            </button>
+          </div>
+        ) : null}
+      </div>
     </>
-  )
+  );
 }
 
-export default FileShareForm
+export default FileShareForm;
